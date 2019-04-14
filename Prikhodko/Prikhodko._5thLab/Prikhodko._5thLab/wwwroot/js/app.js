bca81ad8a98d6174642d40662cf9af604ccdb63a
kirilkin.97 <kirilkin.97@gmail.com>
2019-04-14 16:37:43 +0300

    Add canvas with red color drawing. TODO: fix html columns; add Signal R functionality to drawing

--- Prikhodko/Prikhodko._5thLab/Prikhodko._5thLab/wwwroot/js/app.js
+++ Prikhodko/Prikhodko._5thLab/Prikhodko._5thLab/wwwroot/js/app.js
@@ -1,67 +1,61 @@
-﻿//Problem: No user interaction capability
-//Solution: When user interacts, cause changes appropriately
-var color = $(".selected").css("background-color");;
-var $canvas = $("canvas");
-var context = $canvas[0].getContext("2d");
-var lastEvent;
-var mouseDown = false;
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/charadesHub", {}).build();
+var context = document.getElementById("canvas").getContext("2d");
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint = false;
 
-//When clicking on control list items
-$(".controls").on("click", "li", function () {
-	//Deselect sibling elements
-	$(this).siblings().removeClass("selected");
-	//Select clicked element
-	$(this).addClass("selected");
-	//cache current color
-	color = $(this).css("background-color");
+$('#canvas').mousedown(function (e) {
+	paint = true;
+	addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+	redraw();
+});
+
+$('#canvas').mousemove(function (e) {
+	if (paint) {
+		addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+		redraw();
+	}
+});
+
+$('#canvas').mouseup(function (e) {
+	paint = false;
+});
+
+$('#canvas').mouseleave(function (e) {
+	paint = false;
 });
 
 
-//When "New Color" is pressed
-$("#revealColorSelect").click(function () {
-	//Show color select or hide the color select
-	changeColor();
-	$("#colorSelect").toggle();
-});
-
-//update the new color span
-function changeColor() {
-	var r = $("#red").val();
-	var g = $("#green").val();
-	var b = $("#blue").val();
-
-	$("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
+function addClick(x, y, dragging) {
+	clickX.push(x);
+	clickY.push(y);
+	clickDrag.push(dragging);
 }
 
-//When color sliders change
-$("input[type=range]").change(changeColor)
+function redraw() {
+	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-//When "Add Color" is pressed
-$("#addNewColor").click(function () {
-	//Append the color to the controls ul
-	var $newColor = $("<li></li>");
-	$newColor.css("background-color", $("#newColor").css("background-color"));
-	$(".controls ul").append($newColor);
-	//Select the new color
-	$newColor.click();
-});
+	context.strokeStyle = "#df4b26";
+	context.lineJoin = "round";
+	context.lineWidth = 5;
 
-//On mouse events on the canvas
-$canvas.mousedown(function (e) {
-	lastEvent = e;
-	mouseDown = true;
-}).mousemove(function (e) {
-	//Draw lines
-	if (mouseDown) {
+	for (var i = 0; i < clickX.length; i++) {
 		context.beginPath();
-		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-		context.lineTo(e.offsetX, e.offsetY);
-		context.strokeStyle = color;
+		if (clickDrag[i] && i) {
+			context.moveTo(clickX[i - 1], clickY[i - 1]);
+		} else {
+			context.moveTo(clickX[i] - 1, clickY[i]);
+		}
+		context.lineTo(clickX[i], clickY[i]);
+		context.closePath();
 		context.stroke();
-		lastEvent = e;
 	}
-}).mouseup(function () {
-	mouseDown = false;
-}).mouseleave(function () {
-	$canvas.mouseup();
-});
+}
+
+$('#clearCanvas').click(function() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    clickX = new Array();
+    clickY = new Array();
+    clickDrag = new Array();
+})
